e4f91d1de30ac53929f987dc2e516815a459155e
Angad Tiwari <mac@MACs-iMac.local>
2017-10-04 09:45:13 +0530

    04-oct-2017

---
+++ newsbucket/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  newsbucket
+//
+//  Created by Angad Tiwari on 03/10/17.
+//  Copyright © 2017 angtwr31. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
